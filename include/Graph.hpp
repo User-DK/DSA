f3942d347142ce8b26cec3d76b17bec17071e791
User-DK <daivikkarbhari01@gmail.com>
2024-04-30 22:10:32 +0530

    update

--- include/Graph.hpp
+++ include/Graph.hpp
@@ -4,14 +4,27 @@ using namespace std;
 class Graph
 {
   vector<vector<int>> edgeList;
+  vector<vector<pair<int, int>>> adj;
 
 public:
   Graph() {}
-  void addEdge(int x, int y, int w)
+
+  Graph(int V)
+  {
+    adj.resize(V);
+  }
+
+  void addEdgeK(int x, int y, int w)
   {
     edgeList.push_back({w, x, y});
   }
 
+  void addEdgeP(int x, int y, int w)
+  {
+    adj[x].push_back({w, y});
+    adj[y].push_back({w, x});
+  }
+
   void kruskals_mst(int V);
-  void prims_mst(int V);
+  void prims_mst(int V, int initial);
 };
